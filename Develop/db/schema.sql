fcefb7977f622d0473873ff132c9fbec8f08abb6
unknown <dannysaravia100@gmail.com>
2024-02-05 15:08:45 -0500

    created schema file and addewd the omments

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -4,3 +4,24 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+-- Switch to the newly created database
+
+USE ecommerce_db;
+
+-- establish the category table structure
+
+CREATE TABLE category (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    category_name VARCHAR(255) NOT NULL
+);
+
+-- Dedicate the product table schema
+
+CREATE TABLE product (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    product_name VARCHAR(255) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    stock INT NOT NULL DEFAULT 10,
+    category_id INT,
+    FOREIGN KEY (category_id) REFERENCES category(id)
+)
