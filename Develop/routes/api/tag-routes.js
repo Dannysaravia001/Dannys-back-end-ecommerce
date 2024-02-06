9c303b0992054099a65a16c033d1a3485c713a37
unknown <dannysaravia100@gmail.com>
2024-02-06 03:20:12 -0500

    added the rest of the code for Tag-routes.js and finished

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { endsWith } = require('sequelize/types/lib/operators');
 const { Tag, Product, ProductTag } = require('../../models');
+const { error } = require('formatter');
 
 // The `/api/tags` endpoint
 
@@ -41,16 +42,51 @@ router.get('/:id', (req, res) => {
   });
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+router.post('/', async (req, res) => {
+  try {
+    // Create a new tag
+    const newTag = await Tag.create(req.body);
+    res.status(200).json(newTag);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json(error);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const { id } = req.params;
+    const updatedTag = await Tag.update( req.body, {
+      where: { id: id },
+    });
+    res.status(200).json({ message: 'Tag updated succesfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json(error);
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  router.delete('/:id', async (req, res) => {
+    // delete on tag by its `id` value
+    try {
+      const { id } = req.params; 
+      const deletedRows = await Tag.destroy({
+        where: { id: id },
+      });
+      if (deletedRows > 0 ) {
+        res.status(200).json({ message: 'Tag deleted successfully' });
+      } else { 
+        console.log(error); 
+        res.status(404).json({ message: 'Tag not found'})
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(400).json(error);
+    }
+  });
 });
 
 module.exports = router;
