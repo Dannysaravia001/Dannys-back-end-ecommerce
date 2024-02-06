3a3d871bf64647999adcc0eabd0260a1ba89fae9
unknown <dannysaravia100@gmail.com>
2024-02-06 03:07:14 -0500

    added 2  router.get in tag-routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -1,16 +1,44 @@
 const router = require('express').Router();
+const { endsWith } = require('sequelize/types/lib/operators');
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    // Find all tags and include associated Product data
+    const allTags = await Tag.findAll({ include: [Product] });
+    res.status(200).json(allTags);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  router.get('/:id', async (req, res) => {
+    // find a single tag by its `id`
+    // be sure to include its associated Product data
+    try {
+      const { id } = req.params;
+      const singleTag = await Tag.findOne({
+        where: { id: id }, 
+        include: [Product],
+      });
+      if (singleTag) {
+        res.status(200).json(singleTag);
+      } else {
+        res.status(404).json({ message: 'Tag not found' });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  });
 });
 
 router.post('/', (req, res) => {
